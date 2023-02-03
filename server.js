295072b0483fa03eba9625a09ce73f5a556e12e7
Logan Lentz <logan.lentz1@gmail.com>
2023-02-03 02:44:53 -0600

    have not fixed a thing lol

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const auth0 = require('auth0-js');
 const app = express();
+const dotenv = require('dotenv').config();
+
+
 
 
 app.use(express.static(__dirname + '/public'));
@@ -8,6 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
 
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_CLIENT_ID) {
   throw new Error('Make sure to set AUTH0_DOMAIN and AUTH0_CLIENT_ID in your .env file');
@@ -17,11 +21,3 @@ var webAuth = new auth0.WebAuth({
   domain: process.env.AUTH0_DOMAIN,
   clientID: process.env.AUTH0_CLIENT_ID
 });
-
-function login() {
-  webAuth.authorize({
-    redirectUri: 'http://localhost:3000/callback',
-    responseType: 'token id_token',
-    scope: 'openid profile'
-  });
-}
