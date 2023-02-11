0ffdbb78b826e15aecfb3fee435bddc47d48d99f
Logan Lentz <logan.lentz1@gmail.com>
2023-02-11 02:15:03 -0600

    gitignore

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('./lib/your-compiled-code');
 
 
 
-
+/* 
 app.use(
   auth({
 
@@ -16,14 +16,14 @@ app.use(
     clientID: process.env.CLIENT_ID,
     secret: process.env.SECRET,
   }),
-
-  express.static(__dirname + '/public'));
+*/
+express.static(__dirname + '/public');
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
-
+/*
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_CLIENT_ID) {
   throw new Error('Make sure to set AUTH0_DOMAIN and AUTH0_CLIENT_ID in your .env file');
 }
@@ -32,3 +32,4 @@ var webAuth = new auth0.WebAuth({
   domain: process.env.AUTH0_DOMAIN,
   clientID: process.env.AUTH0_CLIENT_ID
 });
+*/
